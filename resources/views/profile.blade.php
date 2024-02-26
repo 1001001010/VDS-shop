e94e22cc2d591c10dc5479bc6c5f6a9bef1953fd
10010010 <lll100100@yandex.ru>
2024-02-26 17:45:02 +0800

    Информация в профиле

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,23 +1,72 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
+    <section class="second__section second">
+        <div class="container">
+            <div class="tovar">
+                <div class="Offers">
+                    <div class="left_block">
+                        <p class="user__title">Пользователь <span class="bold">{{ Auth::user()->name }}</span></p>
+                        <ul type="circle" class="haracter">
+                            <li>
+                                <p>Email: <span class="bold">{{ Auth::user()->email }}</span></p>
+                            </li>
+                            <li>
+                                <p>Балланс: <span class="bold">{{ Auth::user()->balance }}₽</span></p>
+                            </li>
+                            <li>
+                                <p>Дата регистрации: <span class="bold">{{ Auth::user()->created_at }}</span></p>
+                            </li>
+                            <div class="flex wrapper flex__col__start button__reduct_user gap__15 padding-t__15">
+                                <div class="table__item">
+                                    <a target="_blank">Изменить информацию</a>
+                                </div>
                             </div>
-                        @endif
-
-                        {{ __('You are logged in!') }}
+                        </ul>
+                    </div>
+                    <div class="right_block">
+                        <div class="title-centre">
+                            <p>Всего аренд: <span class="bold">6</span></p>
+                        </div>
+                        <div class="serv__inf__block">
+                            <p>28.06.2023</p>
+                            <p>ID сервера: 23</p>
+                            <p>Срок: Месяц</p>
+                            <p>Цена: 450</p>
+                        </div>
+                        <div class="serv__inf__block">
+                            <p>28.06.2023</p>
+                            <p>ID сервера: 23</p>
+                            <p>Срок: Месяц</p>
+                            <p>Цена: 450</p>
+                        </div>
+                        <div class="serv__inf__block">
+                            <p>28.06.2023</p>
+                            <p>ID сервера: 23</p>
+                            <p>Срок: Месяц</p>
+                            <p>Цена: 450</p>
+                        </div>
+                        <div class="serv__inf__block">
+                            <p>28.06.2023</p>
+                            <p>ID сервера: 23</p>
+                            <p>Срок: Месяц</p>
+                            <p>Цена: 450</p>
+                        </div>
+                        <div class="serv__inf__block">
+                            <p>28.06.2023</p>
+                            <p>ID сервера: 23</p>
+                            <p>Срок: Месяц</p>
+                            <p>Цена: 450</p>
+                        </div>
+                        <div class="serv__inf__block">
+                            <p>28.06.2023</p>
+                            <p>ID сервера: 23</p>
+                            <p>Срок: Месяц</p>
+                            <p>Цена: 450</p>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 @endsection
